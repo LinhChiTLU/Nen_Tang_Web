7728f0182b75b0aa56504d14a2642efca67a45f6
LinhChiTLU <linhchi10012004@gmial.com>
2025-08-03 00:07:32 +0700

    Làm câu 1 và câu 2 Đề 2 bổ sung

---
+++ Nen_Tang_Web/De_2_Cau12/data.js
@@ -0,0 +1,37 @@
+const giaoDichData = [
+  {
+    id: "01",
+    khachHang: "Vũ Thị Linh Chi",
+    nhanVien: "Đỗ Thị Quý Mùi",
+    soTien: 950000,
+    ngayMua: "08/01/2024 10:00"
+  },
+  {
+    id: "02",
+    khachHang: "Nguyễn Văn Hùng",
+    nhanVien: "Phạm Thị Hoa",
+    soTien: 1200000,
+    ngayMua: "12/02/2024 14:30"
+  },
+  {
+    id: "03",
+    khachHang: "Trần Thị Hương",
+    nhanVien: "Nguyễn Đức Mạnh",
+    soTien: 850000,
+    ngayMua: "25/03/2024 09:45"
+  },
+  {
+    id: "04",
+    khachHang: "Lê Minh Tuấn",
+    nhanVien: "Hoàng Thu Trang",
+    soTien: 1000000,
+    ngayMua: "07/04/2024 16:20"
+  },
+  {
+    id: "05",
+    khachHang: "Đặng Thị Mai",
+    nhanVien: "Lưu Văn Hải",
+    soTien: 780000,
+    ngayMua: "19/05/2024 11:10"
+  }
+];
